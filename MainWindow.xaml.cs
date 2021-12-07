c7c35b86a6731c8348db4099fae05e727dc8909e
MeatSoup516 <95587097+MeatSoup516@users.noreply.github.com>
2021-12-07 17:24:42 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Book
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"LAPTOP-38P683PN"; //NOTEBOOK-MAX\SQLEXPRESS KOMP\SQLEXPRESS PC-MAX\SQLEXPRESS
+            builder.InitialCatalog = "Kursbook";
+            builder.IntegratedSecurity = true;
+            ClassBook.connection = new SqlConnection(builder.ConnectionString);
+            MainFrame.Navigate(new StartPage());
+            ClassBook.MainFrame = MainFrame;
+        }
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ StartPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Book
+{
+
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+            ClassBook.MainFrame.Navigate(new UsersSet.AuthPage());
+        }
+
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            ClassBook.MainFrame.Navigate(new AdminSet.AdminAuth());
+        }
+    }
+}
